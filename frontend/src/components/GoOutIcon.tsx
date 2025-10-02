58506ce8505be9c230b83f51610902b97ff8db6a
Alex Borisov <borisovalexdev@gmail.com>
2025-10-02 17:30:05 +0300

    Update Review Component

---
+++ frontend/src/components/GoOutIcon.tsx
@@ -0,0 +1,21 @@
+export const GoOutIcon = () => {
+   return (
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         width="24"
+         height="24"
+         fill="none"
+         viewBox="0 0 24 24"
+         stroke="currentColor"
+         strokeWidth={2}
+         className="text-white"
+      >
+         <path strokeLinecap="round" strokeLinejoin="round" d="M13 11l8-8m0 0h-6m6 0v6" />
+         <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M20 13v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h7"
+         />
+      </svg>
+   );
+};
